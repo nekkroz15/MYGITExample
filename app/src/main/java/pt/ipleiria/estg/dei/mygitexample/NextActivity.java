a89ec4330af11091c438ec38bf768bfce834caec
Tadeu <2140238@my.ipleiria.pt>
2016-09-28 16:47:37 +0100

    Add "Back" buttom from "Next" Activity

--- app/src/main/java/pt/ipleiria/estg/dei/mygitexample/NextActivity.java
+++ app/src/main/java/pt/ipleiria/estg/dei/mygitexample/NextActivity.java
@@ -2,6 +2,7 @@ package pt.ipleiria.estg.dei.mygitexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class NextActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class NextActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_next);
+    }
+
+    public void onClickBack(View view) {
+        finish();
     }
 }
